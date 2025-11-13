614fa36ec203ff5b2bcf394cdcebf9387a30d90e
vanahnn-hub <nguyenvanh2910@gmail.com>
2025-11-13 21:38:08 +0700

    Delete DA201-Session4-Bài3.sql

--- DA201-Session4-Bài3.sql
+++
@@ -1,4 +0,0 @@
-insert into Employees(FirstName,LastName,HireDate,Salary)
-value('Nguyễn','Văn A','2023-11-13',18000000),
-('Nguyễn','Văn B','2024-2-23',9000000),
-('Phạm','David','2025-1-27',4000000);
