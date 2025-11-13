b6a9cec150e69849ed35c426be9888cd50ecffd1
vanahnn-hub <nguyenvanh2910@gmail.com>
2025-11-13 21:38:37 +0700

    Add files via upload

---
+++ DA201-Session4-Bài3.sql
@@ -0,0 +1,4 @@
+insert into Employees(FirstName,LastName,HireDate,Salary)
+value('Nguyễn','Văn A','2023-11-13',18000000),
+('Nguyễn','Văn B','2024-2-23',9000000),
+('David','Phạm','2025-1-27',4000000);
